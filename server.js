1d82e28265f831781486ca3e396431e0662722b1
Fabri <fbernaolab@autonoma.edu.pe>
2026-06-25 15:23:18 -0500

    fix render port binding

--- server.js
+++ server.js
@@ -113,6 +113,8 @@ Usuario: ${message}
 
 });
 
-app.listen(3000, () => {
-    console.log("Servidor en http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor en puerto ${PORT}`);
 });
